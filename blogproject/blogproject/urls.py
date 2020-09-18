79bef1c0dcb9e78e1b56cf121553b3e732a27be9
Seoyeon Ma <seoyeonma1209@gmail.com>
2020-09-18 22:25:49 +0900

    url 정리

--- blogproject/blogproject/urls.py
+++ blogproject/blogproject/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     #기존에는 이런 식으로 프로젝트 관련 url들을 관리했다면 이제는 include를 사용하여 각 앱별로 url을 관리할 수 있게 함.
     #프로젝트의 urls.py로 가서 include로 연결
 
-    # account app도 추가 예정
+    # account app도 추가 예정!
 
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 
